8d27eda8ab2c2f3633243c1d4e6c61a076e9e4db
NW <nickbitbucket@business.chankonabe.co.uk>
2024-01-18 22:42:42 +0000

    Update envelope and harmonic shift controls
    Add null test section

--- audio.js
+++ audio.js
@@ -16,150 +16,6 @@
 
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//Audio Code - creates buffers with audio data according to parameters
-//No knowledge of GUI, only knows about AudioBuffer from WebAudioAPI
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-let harmonics = 1000;//Allows 20Hz to have harmonics up to 20KHz??
-let decayLengthFactor = 1.4;//Decay length ( in samples) is 1.4 times longer than the -60db decay time - allows for longer tail than RT60 alone
-let generatedSampleRate = 0;//Sample rate used to generate current buffers
-// Update method to create a buffer
-function getAudioBuffer(
-    sampleRate, //samples per second
-    frequency, //Hz
-    rootPhaseDelay, //-1..1 => -PI..PI for phase of fundamental
-    SecondHarmonicRelativePhaseDelay, //fraction of rootPhaseDelay for phase of second harmonic
-    oddLevel, //-1..1
-    oddFalloff,//0..2 0 = no falloff, 1 = 1/n amplitude, 2 = 1/n^2 amplitude 
-    evenLevel, //-1..1
-    evenFalloff,//0..2 0 = no falloff, 1 = 1/n amplitude, 2 = 1/n^2 amplitude 
-    attack, //Linear time to get to max amplitude  in seconds
-    decay, // time in seconds to get to 1/1024 (-60db) of start value -> exponential decay
-    envelopeFilter // 1-32 1 = no filter, 32 = 1/32 of heaviest filter
-    ) {
-    //Calculate max delay in samples
-    let delay = Math.abs(rootPhaseDelay) * 0.5 * sampleRate/frequency ;
-    let bufferSize = Math.round(sampleRate * (attack + decayLengthFactor * decay) + delay); //Allow for attack and 1.5* decay time 
-        
-    let delay0 =rootPhaseDelay<0 ? 0 : delay;
-    let delay1 =delay * (rootPhaseDelay<0 ?  1-SecondHarmonicRelativePhaseDelay*2 : SecondHarmonicRelativePhaseDelay*2 ); //Delay first harmonic by 1/2 the phase delay but double the frequency
-    let delayN =  rootPhaseDelay<0 ? delay : 0;
-
-    //Create buffer
-    let audioBuffer = new AudioBuffer({
-        length: bufferSize,
-        sampleRate: sampleRate,
-        numberOfChannels: 1
-      });
-      let b = audioBuffer.getChannelData(0);
-      buildEnvelopeBuffer(sampleRate, bufferSize, attack,decay, envelopeFilter);
-      buildHarmonicSeries(frequency, sampleRate, b, bufferSize, oddLevel, oddFalloff, evenLevel, evenFalloff, delay0, delay1, delayN);
-      return audioBuffer;
-}
-
-//Generate the harmonic series
-function buildHarmonicSeries(frequency, sampleRate, b, bufferSize, oddLevel, oddFalloff, evenLevel, evenFalloff, delay0, delay1, delayN) {
-    let f = frequency;
-    let a = 1;// First harmonic, buffer will be normalised later
-    let nyquist = sampleRate * 0.49;//Nyquist limit less a bit
-    mixInSine(sampleRate, b, bufferSize, f, a, delay0 ); //Add fundamental
-
-    for (let i = 1; i < harmonics; i++) {
-        let isEven = (i+1) % 2 == 0;
-        f += frequency;
-        if (frequency>=nyquist) return;//Nyquist limit
-        let falloff =  Math.pow(i+1,-(isEven ? evenFalloff :oddFalloff));
-        let oe = isEven ? evenLevel : oddLevel;
-        mixInSine(sampleRate, b, bufferSize, f, a * oe * falloff, i == 1 ? delay1 : delayN);
-    }
-}
-
-
-//Generate a single envelope, shared by all harmonics
-let envelopeBuffer = null;
-const root2 = Math.sqrt(2);
-const ln1024 =Math.log(1024);
-function buildEnvelopeBuffer(
-    sampleRate, //samples per second
-    bufferSize,
-    attack, // 0..1  = time in seconds
-    decay, // 0..1) 
-    filter // 1-32 
-    ){
-        //Low pass filter setup
-        let x = 0;
-        let y=0;  
-        let a = 1/filter;
-
-        let isDecay = false;
-        let attackRate = 1 / (attack * sampleRate); //Linear attack
-        let decayLambda = ln1024 / (decay * sampleRate); //Exponential decay -> decay is time in seconds to get to 1/1024 (-60db)of start value
-        let envValues = [0]; // Array to store env values
-        for (let i = 1; i < bufferSize; i++) {
-            if (isDecay){
-                x -= decayLambda*x; 
-            }
-            else 
-            {
-                x += attackRate; 
-                if (x >= 1){
-                    isDecay = true;
-                    x=1;
-                }
-            }
-            //Band limit envelope 1-pole IIR low pass
-            y +=  a * (x - y);
-            envValues.push(y);
-        }
-        envelopeBuffer = envValues;
-}
-
-//Generate a single sine wave and mix into the buffer
-function mixInSine(
-    sampleRate, //samples per second
-    buffer, 
-    bufferSize,
-    frequency, //Hz 
-    amplitude, // 0..1
-    delay //in samples for this frequency
-    ) {
-    let theta = 0;
-    let w = frequency * 2 * Math.PI  / sampleRate;
-    let env = 0;
-    for (let i = 0; i < bufferSize; i++) {
-        if (i > delay)   {
-            //Phase accumulator
-            theta += w;
-
-            buffer[i] += amplitude * envelopeBuffer[env++] * Math.sin(theta) ;
-        }
-    }
-}
-
-function getBufferMax(buffer, bufferSize){
-    let b = buffer.getChannelData(0);
-    let max = 0;
-    for (let i = 0; i < bufferSize; i++) {
-        let val = Math.abs( b[i]);
-        if (val>max) max = val;
-    }
-    return max;
-}
-
-function scaleBuffer(buffer, bufferSize, scale){
-    let b = buffer.getChannelData(0);
-    let max = 0;
-    for (let i = 0; i < bufferSize; i++) {
-        b[i]*=scale;
-    }
-    return max;
-}
-
-
-
-
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //GUI/Audio/WebAudioAPI linking code knows about each area of concern
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -167,6 +23,8 @@ let audioContext = null;
 let sourceNode = null;
 let audioBufferA = null;
 let audioBufferB = null;
+let nullTestBuffer = null;
+let nullTestMax = 0;
 
 function ensureAudioContext(){
     if (!audioContext){
@@ -187,7 +45,7 @@ function play(index) {
     if (changed || generatedSampleRate != audioContext.sampleRate){
         updateBuffersAndDisplay();
     }
-    sourceNode.buffer = index==0 ? audioBufferA : audioBufferB;
+    sourceNode.buffer = index==0 ? audioBufferA : (index==1 ? audioBufferB: nullTestBuffer);
     sourceNode.start(0);
 }
 
@@ -247,23 +105,39 @@ function updateBuffers() {
         second, odd, oddFalloff, even, evenFalloff, attack, decay, envelopeFilter
     );
 
-    //Normalise buffers - but scale by the same amount - find which is largest and scale to +/-0.99
-    let scale = 0.99 / Math.max(getBufferMax(audioBufferA, audioBufferA.length), getBufferMax(audioBufferB, audioBufferB.length));
+    nullTestBuffer = buildNullTest(audioBufferA, audioBufferB);
 
-    scaleBuffer(audioBufferA, audioBufferA.length, scale);
-    scaleBuffer(audioBufferB, audioBufferB.length, scale);
+
+    //Normalise buffers - but scale by the same amount - find which is largest and scale to +/-0.99
+    let scale = 0.99 / Math.max(getBufferMax(audioBufferA), getBufferMax(audioBufferB));
+
+    scaleBuffer(audioBufferA, scale);
+    scaleBuffer(audioBufferB, scale);
+
+    //normalise null test buffer if 
+    let nullMax = getBufferMax(nullTestBuffer);
+    nullTestMax = 20 * Math.log10(nullMax);//convert to dB
+    if (nullTestMax>-100){//avoid scaling if null test is close to silent (>-100db)
+        scaleBuffer(nullTestBuffer, 0.99 / nullMax);
+    }
 
     //console.log("Max amplitude: " + Math.max(getBufferMax(audioBufferA, audioBufferA.length), getBufferMax(audioBufferB, audioBufferB.length)));
 }
 
 function updateDisplay(){
     if (!audioBufferA || !audioBufferB) return;
-    paintBuffer(audioBufferA, audioBufferA.length, "waveformA");
-    paintBuffer(audioBufferB, audioBufferB.length, "waveformB");
+    paintBuffer(audioBufferA, "waveformA");
+    paintBuffer(audioBufferB, "waveformB");
+    paintBuffer(nullTestBuffer, "waveformNull");
+    let nullTest = document.getElementById('nullTestdb');
+    nullTest.textContent = nullTestMax.toFixed(1) + "dB";
 }
 
 
-function paintBuffer(buffer, bufferSize, canvasId){
+function paintBuffer(buffer, canvasId){
+    let b = buffer.getChannelData(0);
+    let bufferSize = buffer.length;
+
     var canvas = document.getElementById(canvasId);
     var ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -271,7 +145,7 @@ function paintBuffer(buffer, bufferSize, canvasId){
     let x = 0;
     let y = canvas.height/2;
     let step = canvas.width / bufferSize;
-    let b = buffer.getChannelData(0);
+
     for (let i = 0; i < bufferSize; i++) {
         ctx.lineTo(x, y + b[i] * y);
         x += step;
@@ -281,6 +155,167 @@ function paintBuffer(buffer, bufferSize, canvasId){
 
  
 
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Audio Code - creates buffers with audio data according to parameters
+//No knowledge of GUI, only knows about AudioBuffer from WebAudioAPI
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+let harmonics = 1000;//Allows 20Hz to have harmonics up to 20KHz??
+let decayLengthFactor = 1.4;//Decay length ( in samples) is 1.4 times longer than the -60db decay time - allows for longer tail than RT60 alone
+let generatedSampleRate = 0;//Sample rate used to generate current buffers
+// Update method to create a buffer
+function getAudioBuffer(
+    sampleRate, //samples per second
+    frequency, //Hz
+    rootPhaseDelay, //-1..1 => -PI..PI for phase of fundamental
+    SecondHarmonicRelativePhaseDelay, //fraction of rootPhaseDelay for phase of second harmonic
+    oddLevel, //-1..1
+    oddFalloff,//0..2 0 = no falloff, 1 = 1/n amplitude, 2 = 1/n^2 amplitude 
+    evenLevel, //-1..1
+    evenFalloff,//0..2 0 = no falloff, 1 = 1/n amplitude, 2 = 1/n^2 amplitude 
+    attack, //Linear time to get to max amplitude  in seconds
+    decay, // time in seconds to get to 1/1024 (-60db) of start value -> exponential decay
+    envelopeFilter // 0-1000 1 = no filter, 1000 = 1/1000 of heaviest filter
+    ) {
+    //Calculate max delay in samples
+    let delay = Math.abs(rootPhaseDelay) * 0.5 * sampleRate/frequency ;
+    let bufferSize = Math.round(sampleRate * (attack + decayLengthFactor * decay) + delay); //Allow for attack and 1.5* decay time 
+        
+    let delay0 =rootPhaseDelay<0 ? 0 : delay;
+    let delay1 =delay * (rootPhaseDelay<0 ?  1-SecondHarmonicRelativePhaseDelay*2 : SecondHarmonicRelativePhaseDelay*2 ); //Delay first harmonic by 1/2 the phase delay but double the frequency
+    let delayN =  rootPhaseDelay<0 ? delay : 0;
+
+    //Create buffer
+    let audioBuffer = new AudioBuffer({
+        length: bufferSize,
+        sampleRate: sampleRate,
+        numberOfChannels: 1
+      });
+      let b = audioBuffer.getChannelData(0);
+      buildEnvelopeBuffer(sampleRate, bufferSize, attack,decay, envelopeFilter);
+      buildHarmonicSeries(frequency, sampleRate, b, oddLevel, oddFalloff, evenLevel, evenFalloff, delay0, delay1, delayN);
+      return audioBuffer;
+}
+
+//Generate the harmonic series
+function buildHarmonicSeries(frequency, sampleRate, b, oddLevel, oddFalloff, evenLevel, evenFalloff, delay0, delay1, delayN) {
+    let f = frequency;
+    let a = 1;// First harmonic, buffer will be normalised later
+    let nyquist = sampleRate * 0.49;//Nyquist limit less a bit
+    bufferSize=b.length;
+    mixInSine(sampleRate, b, f, a, delay0 ); //Add fundamental
+
+    for (let i = 1; i < harmonics; i++) {
+        let isEven = (i+1) % 2 == 0;
+        f += frequency;
+        if (frequency>=nyquist) return;//Nyquist limit
+        let falloff =  Math.pow(i+1,-(isEven ? evenFalloff :oddFalloff));
+        let oe = isEven ? evenLevel : oddLevel;
+        mixInSine(sampleRate, b, f, a * oe * falloff, i == 1 ? delay1 : delayN);
+    }
+}
+
+
+//Generate a single envelope, shared by all harmonics
+let envelopeBuffer = null;
+const root2 = Math.sqrt(2);
+const ln1024 =Math.log(1024);
+function buildEnvelopeBuffer(
+    sampleRate, //samples per second
+    bufferSize,
+    attack, // 0..1  = time in seconds
+    decay, // 0..1) 
+    filter // 0-1000 
+    ){
+        //Low pass filter setup
+        let x = 0;
+        let y=0;  
+        let a = 1/Math.max(1,filter);
+
+        let isDecay = false;
+        let attackRate = 1 / (attack * sampleRate); //Linear attack
+        let decayLambda = ln1024 / (decay * sampleRate); //Exponential decay -> decay is time in seconds to get to 1/1024 (-60db)of start value
+        let envValues = [0]; // Array to store env values
+        for (let i = 1; i < bufferSize; i++) {
+            if (isDecay){
+                x -= decayLambda*x; 
+            }
+            else 
+            {
+                x += attackRate; 
+                if (x >= 1){
+                    isDecay = true;
+                    x=1;
+                }
+            }
+            //Band limit envelope 1-pole IIR low pass
+            y +=  a * (x - y);
+            envValues.push(y);
+        }
+        envelopeBuffer = envValues;
+}
+
+//Generate a single sine wave and mix into the buffer
+function mixInSine(
+    sampleRate, //samples per second
+    buffer, 
+    frequency, //Hz 
+    amplitude, // 0..1
+    delay //in samples for this frequency
+    ) {
+    let theta = 0;
+    let w = frequency * 2 * Math.PI  / sampleRate;
+    let env = 0;
+    let bufferSize = buffer.length;
+    for (let i = 0; i < bufferSize; i++) {
+        if (i > delay)   {
+            //Phase accumulator
+            theta += w;
+
+            buffer[i] += amplitude * envelopeBuffer[env++] * Math.sin(theta) ;
+        }
+    }
+}
+
+function getBufferMax(buffer){
+    let b = buffer.getChannelData(0);
+    let bufferSize = buffer.length;
+    let max = 0;
+    for (let i = 0; i < bufferSize; i++) {
+        let val = Math.abs( b[i]);
+        if (val>max) max = val;
+    }
+    return max;
+}
+
+function scaleBuffer(buffer, scale){
+    let b = buffer.getChannelData(0);
+    let bufferSize = buffer.length;
+    let max = 0;
+    for (let i = 0; i < bufferSize; i++) {
+        b[i]*=scale;
+    }
+    return max;
+}
+
+function buildNullTest(bufferA, bufferB){
+    let length = Math.min(bufferA.length, bufferB.length);
+    var A = bufferA.getChannelData(0);
+    var B = bufferB.getChannelData(0);
+    let nullTest = new AudioBuffer({
+        length: length,
+        sampleRate: bufferA.sampleRate,
+        numberOfChannels: 1
+      });
+      let b = nullTest.getChannelData(0);
+      for (let i = 0; i < length; i++) {
+        b[i] = A[i] - B[i];
+      }
+      return nullTest;
+}
 
 
 
@@ -297,6 +332,9 @@ document.getElementById('playSoundA').addEventListener('click', function() {
 });
 document.getElementById('playSoundB').addEventListener('click', function() {
     play(1);
+});
+document.getElementById('playSoundNull').addEventListener('click', function() {
+    play(2);
 });
 document.getElementById('freq').addEventListener('input', function() {
     document.getElementById('freq-value').textContent = this.value + "Hz";
