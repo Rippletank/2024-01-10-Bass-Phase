972acb1cbb0f9a7c705964f5b658791e4fde56e7
NW <nickbitbucket@business.chankonabe.co.uk>
2024-03-05 18:53:09 +0000

    Refactor FFT functions and add inverse FFT support

--- basicFFT.js
+++ basicFFT.js
@@ -22,7 +22,7 @@
 //wikipedia: https://en.wikipedia.org/wiki/Fast_Fourier_transform
 //https://en.m.wikipedia.org/wiki/Cooley%E2%80%93Tukey_FFT_algorithm
 //https://vanhunteradams.com/FFT/FFT.html#Identifying-a-regression
-//Explanation is great here, but the example code makes it futile to try implementing from scratch! 
+//Explanation is great here, and the example code makes it futile to try implementing from scratch, so it is adapted here.
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
@@ -36,7 +36,8 @@ import { zeroLevel } from "./defaults.js";
 let getFFT1024 = initFFT(1024);
 let getFFT64k = initFFT(65536);
 
-let fftFunctions=[null,null,null,null,null,null,null,null]
+//cache functions to avoid reinitialising
+let fftFunctions=new Array(8).fill(null)
 function getFFTFunction(bufferSize){
     let index =0;
     switch(bufferSize){ 
@@ -81,7 +82,7 @@ function initFFT(N)
     }
 
     //Sin LUT precalculation
-    const sinLUT = new Array(N);
+    const sinLUT = new Float32Array(N);
     const w =2*Math.PI/N;//rads per sample
     for(let i=0;i<N;i++){
         sinLUT[i] = Math.sin(w*i) * 0.5;
@@ -90,8 +91,8 @@ function initFFT(N)
     return (buffer)=>{  
         if (buffer.length!=N)  return null;
         
-        const fr = buffer.slice();
-        const fi = new Array(N).fill(0);
+        const fr = new Float32Array(buffer);
+        const fi = new Float32Array(N);//already initialised to 0
         for(let i=0;i<bitReversals.length;i++){
             const br = bitReversals[i];
             const m = br[0];
@@ -149,5 +150,119 @@ function initFFT(N)
 }
 
 
+//cache functions to avoid reinitialising
+let ifftFunctions= new Array(10).fill(null)
+function getInverseFFTFunction(bufferSize){
+    let index =0;
+    switch(bufferSize){ 
+        case 128: index = 0; break;   
+        case 256: index = 1; break;   
+        case 512: index = 2; break;   
+        case 1024:index = 3; break;    
+        case 2048: index = 4; break;   
+        case 4096: index = 5; break;   
+        case 8192: index = 6; break;    
+        case 16384: index = 7; break;   
+        case 32768: index = 8; break;   
+        case 65536: index = 9; break;   
+        default: return null;
+    }
+    return ifftFunctions[index] ??  (ifftFunctions[index] = initInverseFFT(bufferSize))
+}
 
-export { getFFTFunction, getFFT1024, getFFT64k };
+
+
+function initInverseFFT(N)
+{
+    //FFT will always be of length N so bit reversals and sin lUT can be precalculated
+    const N_1 =N-1;
+    const N_2 =N/2;
+    const N_4 =N/4;
+    const logN = Math.log2(N);
+    const shift = 16-logN;
+    let bitReversals = [];
+    //Bit reversal precalculation
+    //https://vanhunteradams.com/FFT/FFT.html#Generalized-code
+    //who referenced https://graphics.stanford.edu/~seander/bithacks.html#BitReverseObvious
+    //only store needed reversals - not when m==mr 
+    let mr = 0;
+    for(let m=1;m<N_1;m++){
+        mr =((m>>1)  & 0x5555)|((m  & 0x5555)<<1);
+        mr =((mr>>2) & 0x3333)|((mr & 0x3333)<<2);
+        mr =((mr>>4) & 0x0f0f)|((mr & 0x0f0f)<<4);
+        mr =((mr>>8) & 0x00ff)|((mr & 0x00ff)<<8);
+        mr = mr>>shift;
+        if (mr<=m) continue;
+        bitReversals.push([m,mr]);
+    }
+
+    //Sin LUT precalculation
+    const sinLUT = new Array(N);
+    const w =2*Math.PI/N;//rads per sample
+    for(let i=0;i<N;i++){
+        sinLUT[i] = Math.sin(w*i) * 0.5;
+    }
+
+    return (real, img)=>{  
+        if (real.length!=N_2 || img.length!=N_2)  return null;
+        
+        const fr = new Float32Array(N);
+        const fi = new Float32Array(N);
+        //Copy in real and imaginary parts, and reverse part above nyquist
+        //First parts that only appear once
+        fr[0] = real[0];//DC
+        fi[0] = img[0];//DC
+        fr[N_2] = real[N_2];//Nyquist point
+        fi[N_2] = img[N_2];//Nyquist point
+        //Next rest of parts that are reflected
+        for(let i=1;i<N_2;i++){
+            fr[i] = real[i];
+            fi[i] = img[i];
+            fr[N-i] = real[i];
+            fi[N-i] = -img[i];
+        }
+
+        for(let i=0;i<bitReversals.length;i++){
+            const br = bitReversals[i];
+            const m = br[0];
+            const mr = br[1];
+            const tr = fr[m];
+            fr[m] = fr[mr];
+            fr[mr] = tr;
+            const ti = fi[m];//reverse imaginary part, too
+            fi[m] = fi[mr];
+            fi[mr] = ti;
+        }
+        
+
+        //From https://vanhunteradams.com/FFT/FFT.html#Generalized-code
+        //who referenced om Roberts 11/8/89 and Malcolm Slaney 12/15/94 malcolm@interval.com
+        let L=1;
+        let k=logN-1;
+        while (L<N){
+            let iStep = L*2;
+            for(let m=0;m<L;m++){
+                const theta = m<<k;
+                const wr = sinLUT[theta+N_4];//cosine * 0.5 in LUT
+                const wi = sinLUT[theta];//sine * 0.5 in LUT - sign flipped from iFFT
+                for(let i=m;i<N;i+=iStep){
+                    const j=i+L;
+                    let tr = wr*fr[j]-wi*fi[j];
+                    let ti = wr*fi[j]+wi*fr[j];
+                    let qr = fr[i]; //multiplication by 0.5 removed for iFFT
+                    let qi = fi[i]; //multiplication by 0.5 removed for iFFT
+                    fr[j] = qr-tr;
+                    fi[j] = qi-ti;
+                    fr[i] = qr+tr;
+                    fi[i] = qi+ti;
+                }
+            }
+            k-- ;
+            L = iStep ;
+        }
+
+        return fr; //Only real part is needed
+    }
+}
+
+export { getFFTFunction, getInverseFFTFunction, getFFT1024, getFFT64k };
